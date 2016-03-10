1d6a75bd2f1edbebbb4f0ec058173bc39d35b00b
Mi-Skal <mi.skal2467@gmail.com>
2016-03-10 20:57:26 +0200

    Added files via upload

---
+++ Convex Hull.py
@@ -0,0 +1,51 @@
+# Convex hull of a random set of points:
+
+from scipy.spatial import ConvexHull
+import numpy as np
+from math import sqrt
+
+points = np.random.rand(30, 2)   # 30 random points in 2-D
+hull = ConvexHull(points)
+
+maxX = np.amax(points[:1])
+maxY = np.amax(points[:2])
+minX= np.amin(points[:1])
+minY = np.amin(points[:2])
+
+
+a = sqrt((( minX-maxX)**2) + ((minY-minY)**2))
+b = sqrt((( maxX-maxX)**2) + ((maxY-minY)**2))
+
+
+# Plot it:
+
+import matplotlib.pyplot as plt
+plt.plot(points[:,0], points[:,1], 'o')
+for simplex in hull.simplices:
+    plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
+
+
+plt.plot(points[hull.vertices,0], points[hull.vertices,1], 'r--', lw=2)
+plt.plot(points[hull.vertices[0],0], points[hull.vertices[0],1], 'ro')
+
+
+def in_hull(p, hull):
+	from scipy.spatial import Delaunay
+	if not isinstance(hull,Delaunay):
+		hull = Delaunay(hull)
+		
+	return hull.find_simplex(p)>=0
+
+randpoints = np.random.rand(1000,2)
+
+table = in_hull(randpoints,points)
+
+x = np.count_nonzero(table)
+
+E = (x/1000.0)*a*b
+print "The Area of the Convex Hull is:" , E
+
+
+
+
+		
