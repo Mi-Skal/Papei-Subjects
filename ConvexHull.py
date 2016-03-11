77c7c78067b392c0e2b4d97a011dc43083f83d79
Mi-Skal <mi.skal2467@gmail.com>
2016-03-11 16:44:27 +0200

    Update ConvexHull.py

--- ConvexHull.py
+++ ConvexHull.py
@@ -10,10 +10,10 @@ hull = ConvexHull(points)
 maxp = np.amax(points , axis = 0)
 minp = np.amin(points , axis = 0)
 
-maxX = np.amax(maxp[:1])
-maxY = np.amax(maxp[:2])
-minX = np.amin(minp[:1])
-minY = np.amin(minp[:2])
+maxX = maxp[0]
+maxY = maxp[1]
+minX = minp[0]
+minY = minp[1]
 
 
 a = sqrt((( minX-maxX)**2) + ((minY-minY)**2))
